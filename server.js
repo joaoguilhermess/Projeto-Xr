78e0c7ffd273e454b507f619ad310718236a95d2
Jotagê <71993919+JG-entusiasta@users.noreply.github.com>
2022-11-23 18:58:16 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const https = require("https");
+const path = require("path");
+const fs = require("fs");
+
+var app = express();
+
+app.get("/", function(req, res) {
+	res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/whitney.fnt", function(req, res) {
+	res.sendFile(path.join(__dirname, "whitney.fnt"));
+});
+
+app.get("/whitney.png", function(req, res) {
+	res.sendFile(path.join(__dirname, "whitney.png"));
+});
+
+app.get("/untitled.glb", function(req, res) {
+	res.sendFile(path.join(__dirname, "untitled.glb"));
+});
+
+app.get("/paper.png", function(req, res) {
+	res.sendFile(path.join(__dirname, "paper.png"));
+});
+
+var server = https.createServer({
+	key: fs.readFileSync("https.key"),
+	cert: fs.readFileSync("https.cert")
+}, app);
+
+server.listen(3000, function() {
+	console.log("Ready");
+});
